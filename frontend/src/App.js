aa25bfa14d396af7a8ebe3c13f3f6866b5b52322
Łukasz Wójcik <wojcik.l@tlen.pl>
2023-03-26 19:21:04 +0200

    Main page configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainPage from './components/MainPage';
+import './assets/css/tailwind.css';
 
 function App() {
   return (
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -21,13 +21,37 @@ function MainPage() {
     };
 
     return (
-        <div>
-            <h1>Roll The Dice RPG</h1>
-            <p>Roll The Dice RPG to prosta przeglądarkowa gra RPG wykorzystująca mechanikę D&D</p>
-            <h2>O mnie</h2>
-            <p>Elo mordko</p>
-            <button onClick={handleLoginClick}>Log in</button>
-            <button onClick={handleRegisterClick}>Sign up</button>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">ROLL THE DICE RPG</h1>
+            <p className="max-w-md text-center mb-8">
+                Roll The Dice RPG to prosta przeglądarkowa gra RPG
+                wykorzystująca mechanikę D&D
+            </p>
+            <div className="text-center">
+                <button
+                    className="px-4 py-2 rounded-md bg-blue-500 text-white mr-4"
+                    onClick={handleLoginClick}
+                >
+                    Log in
+                </button>
+                <button
+                    className="px-4 py-2 rounded-md bg-green-500 text-white"
+                    onClick={handleRegisterClick}
+                >
+                    Sign up
+                </button>
+            </div>
+            <hr className="w-1/2 border-gray-300 my-8" />
+            <div className="max-w-md">
+                <h2 className="text-xl font-bold">O mnie</h2>
+                <p className="my-4">
+                    Elo mordko. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Sed auctor elit ac quam vestibulum
+                    bibendum. Suspendisse potenti. Maecenas rutrum dui vel justo
+                    lobortis pellentesque. Duis lobortis nunc ut neque laoreet
+                    malesuada.
+                </p>
+            </div>
         </div>
     );
 }
